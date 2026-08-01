4394224c0675648cdb4dc6031b97ea7c6d0aa241
Babs Creations 4U <msbgoins@gmail.com>
2026-08-01 07:34:20 +0000

    Coomit changes for deleted files and added new files for project restructuring and documentation.

--- reviews.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getAllReviews, createReview } from '../controllers/reviewController.js';
-
-const router = Router();
-router.get('/', getAllReviews);
-router.post('/', createReview);
-
-export default router;
